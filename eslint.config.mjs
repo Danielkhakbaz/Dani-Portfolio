304838e8b9a5306715bd9cc78e5bf562d6656d8d
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-02 15:14:56 +0330

    (chore): Update the dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import globals from "globals";
 import react from "eslint-plugin-react";
+import next from "@next/eslint-plugin-next";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPretier from "eslint-config-prettier";
 
 export default [
   {
@@ -34,11 +36,21 @@ export default [
       react,
     },
     rules: {
+      ...eslintConfigPretier.rules,
       "react/jsx-no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": ["error"],
       "react/react-in-jsx-scope": 0,
       "spaced-comment": ["error", "always", { markers: ["/"] }],
       "new-cap": 0,
+    },
+  },
+  {
+    plugins: {
+      "@next/next": next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs["core-web-vitals"].rules,
     },
   },
 ];
